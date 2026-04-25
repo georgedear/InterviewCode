5e21f454ca53f665da46ab3fac554e3767057801
George <georgewilliamdear@gmail.com>
2026-04-25 11:40:36 +0100

    Update

--- Frontend/currency-app/src/app/app.component.ts
+++ Frontend/currency-app/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { ExchangeRateViewer } from './exchange-rate-viewer/exchange-rate-viewer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [ExchangeRateViewer],
+  template: `
+    <exchange-rate-viewer/>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'currency-app';
+
+  // TODO: Query for all currencies on load/construction
+  // TODO: Push currency codes into store
+  // TODO: Pluck currency codes from store and inject into component
+
+
+
+  // TODO: Read this from state once implemented
+  public currencyCodes = signal<string[]>(['USD', 'GBP', 'EUR'])
+
 }
---
+++ Frontend/currency-app/src/app/exchange-rate-viewer/exchange-rate-viewer.component.ts
@@ -0,0 +1,33 @@
+import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'exchange-rate-viewer',
+    standalone: true,
+    imports: [
+        CommonModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatSelectModule
+    ],
+    template: `
+        @let codes = currencyCodes();
+
+        <mat-form-field>
+            <mat-label>Favorite food</mat-label>
+            <mat-select>
+                @for (code of codes; track code) {
+                <mat-option [value]="code">{{ code }}</mat-option>
+                }
+            </mat-select>
+        </mat-form-field>
+    `
+})
+export class ExchangeRateViewer {
+
+    currencyCodes = input<string[]>();
+}
